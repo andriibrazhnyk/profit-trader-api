76b6ccb064a8e25a66168691156cbd25317d912c
Andrii Brazhnyk <doctravik@gmail.com>
2025-07-08 23:01:16 +0300

    chore: add Swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,16 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Forex API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+}
